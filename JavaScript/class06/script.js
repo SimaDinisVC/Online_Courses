9dff41d0163735214dbdd66e978cc2b46f227fe2
Simão Costa <simadinisvc@gmail.com>
2021-12-18 15:11:35 +0000

    First Commit JS Course Exercises

---
+++ JavaScript/class06/script.js
@@ -0,0 +1,4 @@
+var nome = prompt('Qual é o seu nome?');
+document.write(`Olá ${nome} seu nome tem ${nome.length} letras.<br>`)
+document.write(`Seu nome em maiscúlas é ${nome.toUpperCase()}.<br>`)
+document.write(`Seu nome em minúsculas é ${nome.toLowerCase()}.<br>`)
